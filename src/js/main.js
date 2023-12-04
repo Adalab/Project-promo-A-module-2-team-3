64f642064ee56e722850db53b05f79eca3fce631
BMES89 <bessyescobar89@gmail.com>
2023-12-04 20:11:38 +0100

    adding partial for js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//hay que decir el orden en el que tiene quedar
+
+import './collapsible.js';
+import './preview-content.js';
+import './preview-palettes.js';
+import './reset-button.js';
+import './share.js';
