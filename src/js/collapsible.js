480caa7d9034bb5ff2f9fa4567178d1103a9383e
larafdezmorena <larafdezmorena@gmail.com>
2023-12-08 22:38:15 +0100

    add collapsibles function

--- src/js/collapsible.js
+++ src/js/collapsible.js
@@ -2,3 +2,28 @@
 
 const collapsibleBtn = document.querySelectorAll(".js-collapsible--btn");
 
+for (const item of collapsibleBtn) {
+    item.addEventListener("click", updateCollapsible);
+}
+function updateCollapsible(ev) {
+    const currentCollapsible = ev.currentTarget;
+    if(currentCollapsible.children[2].classList.contains('fa-chevron-up')){
+        currentCollapsible.children[2].classList.remove('fa-chevron-up');
+        currentCollapsible.children[2].classList.add('fa-chevron-down');
+        currentCollapsible.nextElementSibling.classList.remove('collapsible__colours--hide');
+        currentCollapsible.nextElementSibling.classList.add('collapsible__colours--show');
+    }
+    else{
+        currentCollapsible.children[2].classList.remove('fa-chevron-down');
+        currentCollapsible.children[2].classList.add('fa-chevron-up');
+        currentCollapsible.nextElementSibling.classList.remove('collapsible__colours--show');
+        currentCollapsible.nextElementSibling.classList.add('collapsible__colours--hide');
+    }
+
+    /*if (currentCollapsible.classList.contains("collapsable--open")) {
+      currentCollapsible.classList.remove("collapsable--open");
+    } else {
+      currentCollapsible.classList.add("collapsable--open");
+    }*/
+}
+
