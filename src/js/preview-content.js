f10637e8aafeef61a87cbf8226eac149435102b0
BMES89 <bessyescobar89@gmail.com>
2023-12-09 22:52:06 +0100

    first try JS wins

--- src/js/preview-content.js
+++ src/js/preview-content.js
@@ -1,1 +1,37 @@
 console.log('preview-content');
+
+const createButton = document.querySelector('.js__createButton');
+
+const noClickedButton = document.querySelector('.share-container__button--styles');
+const clickedButton = document.querySelector('.share-container__button--styles--grey');
+
+const createCard = document.querySelector('.js_createCard');
+
+
+//para cambiar el color del boton
+clickedButton.addEventListener('clicked' , (event) => {
+    // event.preventDefault();
+
+    createCard.classList.remove('share-container__button--styles--grey' , 'share-container__button--styles');
+    previewCard.classList.add('share-container__button--styles--grey');
+});
+
+noClickedButton.addEventListener( 'click' , (event) => {
+    // event.preventDefault();
+
+    createCard.classList.remove('share-container__button--styles--grey' , 'share-container__button--styles');
+    previewCard.classList.add('share-container__button--styles');
+});
+
+//para que se extienda el mensaje "La tarjeta ha sido creada"
+function togglecreateCard() {
+    createCard.classList.toggle('hidden');
+    console.log('funciona')
+};
+
+createButton.addEventListener( 'click' , (event) => {
+    event.preventDefault();
+    createButton.classList.toggle('clicked');
+    togglecreateCard
+ console.log('funciona')
+});
