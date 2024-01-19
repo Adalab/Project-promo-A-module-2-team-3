41cae69785c8e75fb22e82cd2d48f970840d2050
AntiaNavajasGarrido <antianavajasgarrido@gmail.com>
2024-01-19 20:20:25 +0100

    adding more changes to CSS JS and HTML

--- src/js/preview-palettes.js
+++ src/js/preview-palettes.js
@@ -10,11 +10,7 @@ const palette3 = document.querySelector('.js-palette3');
 palette1.addEventListener('input', (event) => {
     data.palette = 1;
 
-<<<<<<< HEAD
     previewCard.classList.remove('palette2', 'palette3')
-=======
-    previewCard.classList.remove('palette1' , 'palette2' , 'palette3')
->>>>>>> 0b9c111ac1f9b34de80d4d49699b225a062cdf3d
 
     previewCard.classList.add('palette1')
 });
@@ -22,11 +18,7 @@ palette1.addEventListener('input', (event) => {
 palette2.addEventListener('input', (event) => {
     data.palette = 2;
 
-<<<<<<< HEAD
     previewCard.classList.remove('palette1', 'palette3')
-=======
-    previewCard.classList.remove('palette1' , 'palette2' , 'palette3')
->>>>>>> 0b9c111ac1f9b34de80d4d49699b225a062cdf3d
 
     previewCard.classList.add('palette2')
 });
@@ -34,11 +26,7 @@ palette2.addEventListener('input', (event) => {
 palette3.addEventListener('input', (event) => {
     data.palette = 3;
 
-<<<<<<< HEAD
     previewCard.classList.remove('palette1', 'palette2')
-=======
-    previewCard.classList.remove('palette1' , 'palette2' , 'palette3')
->>>>>>> 0b9c111ac1f9b34de80d4d49699b225a062cdf3d
 
     previewCard.classList.add('palette3')
 });
