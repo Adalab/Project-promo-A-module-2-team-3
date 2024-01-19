a8ef0e944087a095730685805b966c2e1e323ad5
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-01-19 19:33:24 +0100

    add creating fetch

--- src/js/share.js
+++ src/js/share.js
@@ -13,6 +13,35 @@ const previewLinkedin = document.querySelector(".js_linkedin");
 const githubInput = document.querySelector(".js_githubInput");
 const previewGithub = document.querySelector(".js_github");
 
+const btnCreateCard = document.querySelector('.js_btnCreateCard');
+const shareResponseContainer = document.querySelector('.js_shareLinkContainer');
+const js_shareLinkContainer = document.querySelector('.js_shareLinkContainer');
+
+
+function handleClickCreate (event){
+event.preventDefault()
+console.log('crear tarjeta');
+
+fetch('https://dev.adalab.es/api/card/', {
+  method: 'POST',
+  headers: {'Content-Type': 'application/json' },
+  body: JSON.stringify(data)
+})
+.then(response => response.json())
+.then(dataResponse =>{
+  console.log(dataResponse)
+
+  if ( dataResponse.success ) {
+    
+
+  } else{
+    //mostrar mensaje de error 
+  }
+
+})
+}
+
+
 nameInput.addEventListener("input", (event) => {
   data.name = event.currentTarget.value;
   previewFullName.innerHTML = data.name;
@@ -43,3 +72,6 @@ githubInput.addEventListener("input", (event) => {
   data.github = event.currentTarget.value;
   previewGithub.href = `https://github.com/${data.github}`;
 });
+
+
+btnCreateCard.addEventListener ('click', (handleClickCreate));
